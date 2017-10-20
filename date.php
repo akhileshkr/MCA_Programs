4aadf7cee420e31ba4354d52acd128b0d9dfe481
akhileshkr <akhilesh0614@gmail.com>
2017-10-20 21:37:32 +0530

    programs

--- date.php
+++ date.php
@@ -1,17 +1,20 @@
 <html>
 <body>
-<form method="get">
+<form method="post">
 <center>
 
 <input type="submit" value="Click" name="bt1"/><u>Here for date formats:</u><br>
 <?php
-if($_GET['bt1'])
+if(isset($_POST['bt1']))
+{
+if($_POST['bt1'])
 {
 echo "Date In Different Format"."<br>";
 echo "Today is " . date("d/m/y") . "<br>";
 echo "Today is " . date("D-M-Y") . "<br>";
 echo "Today is " . date(" l  dS M Y") . "<br>";
 echo "Today is " . date(" l dS \of F Y") . "<br>";
+}
 }
 ?>
 </center>
